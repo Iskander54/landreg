bbb097bccfa79b2a8b9a7d8b9f507ed17247cde9
Alex-Kevin LOEMBE <alexkevin.loembe@nist.gov>
2019-12-02 14:51:07 -0500

    adding identifiers

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.5.0;
 
 contract Registry {
 
+address payable public owner;
 /* Data model that allows me to have a stucts with delete and index */
   struct Property {
     address owner;
@@ -13,9 +14,16 @@ contract Registry {
   mapping(uint => Property) public properties;
   uint[] public propertyList;
 
+  modifier isContractOwner(){ require(msg.sender == owner,"Only owner of the contract can call this function");_;}
+  modifier isPropertyOwner(uint pin){ require(msg.sender == properties[pin].owner,"Only owner of the property can call this function");_;}
+
+constructor() public{
+  owner=msg.sender;
+}
+
 /* Function that check if a PIN (property identification number) exists */
   function isProperty(uint pin) public view returns(address) {
-    require(propertyList.length>0,"There is no property yet");
+    //require(propertyList.length>0,"There is no property yet");
     if(propertyList[properties[pin].listPointer] == pin) {
          return properties[pin].owner;
      }else{
@@ -29,21 +37,21 @@ contract Registry {
   }
   
 /*function that create a new property on the blockchain */
-  function newProperty(address ownerAddress, uint pin) private returns(bool success) {
-    require(isProperty(pin)==address(0),"This PIN already exist");
+  function newProperty(address ownerAddress, uint pin) public returns(bool success) {
+    //require(isProperty(pin)==address(0),"This PIN already exist");
     properties[pin].owner = ownerAddress;
     properties[pin].listPointer = propertyList.push(pin) - 1;
     return true;
   }
 /* Function that allows to change the owner of a property */
-  function updateProperty(address ownerAddress, uint pin) private returns(bool success) {
-    require(isProperty(pin)!=address(0),"This PIN doens't exist");
+  function updateProperty(address ownerAddress, uint pin) public isContractOwner() returns(bool success) {
+    //require(isProperty(pin)!=address(0),"This PIN doens't exist");
     properties[pin].owner = ownerAddress;
     return true;
   }
 /*function that allow to delete a property on the blockchain */
-  function deleteProperty(uint pin) private returns(bool success) {
-    require(isProperty(pin)!=address(0),"This PIN doens't exist");
+  function deleteProperty(uint pin) public isContractOwner() isPropertyOwner(pin) returns(bool success) {
+    //require(isProperty(pin)!=address(0),"This PIN doens't exist");
     uint rowToDelete = properties[pin].listPointer;
     uint keyToMove   = propertyList[propertyList.length-1];
     propertyList[rowToDelete] = keyToMove;
--- test/registry.test.js
+++ test/registry.test.js
@@ -1,29 +1,58 @@
 // The public file for automated testing can be found here: https://gist.github.com/ConsenSys-Academy/e9ec0d8d6c53b56ca9673cfa139b5644
 
 var Registry = artifacts.require('Registry')
-//let catchRevert = require("./exceptionsHelpers.js").catchRevert
+let catchRevert = require("./exceptionsHelpers.js").catchRevert
 const BN = web3.utils.BN
 
 contract('Registry',function(accounts){
     
     const alex=accounts[0]
-    const kevin=accounts[0]
+    const kevin=accounts[1]
 
     beforeEach(async () => {
         instance = await Registry.new()
     })
-    it("should add two property to the blockchain",async()=>{
-        const tx =await instance.newProperty(alex,5)
+
+    it("Check new property is created",async()=>{
+        const add=await instance.newProperty(alex,3)
+        const check= await instance.isProperty(3)
+
+        assert.equal(check,alex,"new property is not found")
+    })
+
+    it("Should add two property to the blockchain and count them",async()=>{
+        const tx1=await instance.newProperty(alex,5)
         /*
         if (tx.logs[0].event == "PropertyAdd") {
             eventEmitted = true
             console.log[tx.logs[0].value]
         }*/
-        const txx=await instance.newProperty(kevin,2)
+        const tx2=await instance.newProperty(kevin,2)
         /*
         if (txx.logs[0].event == "PropertyAdd") {
             eventEmitted = true
         }*/
-        assert.equal(instance.getPropertyCount(),3,'the number of accounts is the same as the number of added accounts')
+        const nb = await instance.getPropertyCount()
+        assert.equal(nb,2,'the number of accounts is the same as the number of added accounts')
+    })
+
+    it("Update a property that has been added ",async()=>{
+        const add=await instance.newProperty(alex,3)
+        const checkAdded= await instance.isProperty(3)
+        if(checkAdded==alex){
+            const update=await instance.updateProperty(kevin,3)
+        }
+        const checkUpdate = await instance.isProperty(3)
+        assert.equal(checkUpdate,kevin,"The property hasnt changed owner")
+    })
+
+    it("Delete a property that has been added ",async()=>{
+        const add=await instance.newProperty(alex,3)
+        const checkAdded= await instance.isProperty(3)
+        if(checkAdded==alex){
+            const update=await instance.deleteProperty(3,{from: kevin})
+        }
+        const nb = await instance.getPropertyCount()
+        assert.equal(nb,0,"The property hasnt changed owner")
     })
 })
