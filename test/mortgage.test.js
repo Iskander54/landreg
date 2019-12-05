4cc4c4a742b3720e8d7db3618430300a85208804
Alex-Kevin LOEMBE <alexkevin.loembe@nist.gov>
2019-12-05 16:18:39 -0500

    add one test

--- test/mortgage.test.js
+++ test/mortgage.test.js
@@ -5,5 +5,31 @@ let catchRevert = require("./exceptionsHelpers.js").catchRevert
 const BN = web3.utils.BN
 
 contract('Mortgage',function(accounts){
-    
+
+    const bank=accounts[0]
+    const client=accounts[1]
+    const prop_owner=accounts[2]
+    const other=accounts[3]
+
+    beforeEach(async()=>{
+        instance = await Mortgage.new();
+    })
+
+    it("Check that the transaction is executed when all parties have sign the transaction",async()=>{
+
+    })
+
+    it("Unable to confirm a transaction if you are not a party",async()=>{
+
+    })
+
+    it("Client or Bank able to revoke contract within 14 days ",async()=>{
+
+    })
+
+    it("not able to process transaction if not all requirement met",async()=>{
+        
+    })
+
+
 })
