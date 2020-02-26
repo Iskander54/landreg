daab89baa1d9203a1a65d40cd0e3ec36bba8999f
Alex-Kevin LOEMBE <alexkevin.loembe@nist.gov>
2020-02-26 15:49:23 -0500

    Repayment contract, start testing, still little things to deal with

--- contracts/Repayment.sol
+++ contracts/Repayment.sol
@@ -1,91 +1,109 @@
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 pragma solidity ^0.5.0;
 
-contract Repayment{
+contract Registry {
+    function updatePropertyFromMortgage(address ownerAddress, uint256 pin) public returns(bool success);
+}
 
-    
+contract Repayment{
+    using SafeMath for uint256;
+    uint256 ETHER=(10**18);
     address creditor;
     address creditee;
-    uint originalamount;
-    uint rates;
-    uint length;
-    uint balance;
-    uint pin; 
+    uint256 originalamount;
+    uint256 rates;
+    uint256 length;
+    uint256 public balance;
+    uint256 pin; 
 
-    uint missedPayment=0;
-    uint256 paymentPeriod = 7 days;
-    uint256 dueDate = now + paymentPeriod;
+    uint256 public missedPayment=0;
+    uint256 public paymentPeriod = 7 days;
+    uint256 public dueDate = now + paymentPeriod;
+    address Reg;
+    uint256 public penalty=0;
+
+    event penal(uint256 p);
+
 
 
     constructor(
         address _creditor,
         address _creditee,
-        uint _originalamount,
-        uint _rates,
-        uint _length,
-        uint _balance
+        uint256 _originalamount,
+        uint256 _rates,
+        uint256 _length,
+        address _reg
     ) public {
         creditor = _creditor;
         creditee = _creditee;
-        originalamount = _originalamount;
-        rates = div(_rates,100);
+        originalamount = _originalamount*ETHER;
+        rates = _rates; //to be divided by 100 bc solidity dont deal with float
         length = _length;
-        balance = _balance;
-
+        balance = originalamount;
+        Reg=_reg;
     }
 
-    function calculateComponents(uint256 amount)internal view{
-        interest = mul(balance,rates)
+    function calculateComponents(uint256 amount)internal view returns(uint256,uint256){
+        uint256 tmp = SafeMath.mul(balance,rates);
+        uint256 interest = SafeMath.div(tmp,100);
         require(amount>=interest,"The minimun payment is the interest per period");
-        principal = amount - interest;
-        return (interest,principal)
+        uint256 principal = amount - interest;
+        return (interest,principal);
     }
 
-    function minimumPayment() public view {
-        return mul(balance,rates)
+    function minimumPayment() public view returns (uint256){
+        return balance.mul(rates);
 
     }
 
-    function processPeriod(uint principal) internal {
+    function processPeriod(uint256 principal) internal {
         balance-=principal;
-        dueDate=+= paymentperiod;
+        dueDate+= paymentPeriod;
+    }
+
+    /// @dev allows anyone to check the balance of the contract
+    /// @return returns the contract balance
+    function getNow() public view returns (uint256){
+        return now;
     }
 
 
-    function makePayment() public {
+    function makePayment() public payable {
         if(now>dueDate){
-            MissedPayment()
+            MissedPayment();
+            uint256 change = msg.value-penalty;
+            msg.sender.transfer(change);
         }else{
-        uint256 interest;
-        uint256 principal;
-        (interest,principal) = calculatePrincipal(msg.value);
-        require(principal<=remainingBalance,"You can't pay more than what you owe");
-        processPeriod(principal)
+        (uint256 interest,uint256 principal) = calculateComponents(msg.value);
+        require(principal<=balance,"You can't pay more than what you owe");
+        require(msg.value >=interest || principal == balance, "payment is at least interest amount or maximum balance amount+interest ");
+        processPeriod(principal);
         }
 
     }
 
-    function MissedPayment() public{
-        requires(now>dueDate);
+    function MissedPayment() public {
+        require(now>dueDate);
         if(missedPayment<4){
-            interest = mul(balance,rates)
-            fees = div(missedPayment,100)
-            penalty = mul(fees,interest )
-            missedPayment+=1
-            return(interest,penalty)
+            missedPayment+=1;
+            uint256 tmp = SafeMath.mul(balance,rates);
+            uint256 interest = SafeMath.div(tmp,100);
+            uint256 fees = SafeMath.mul(missedPayment,interest);
+            fees = SafeMath.div(fees,100);
+            penalty = interest+fees;
+            emit penal(fees);
+            emit penal(interest);
         }else{
             cancelLoan();
-            return 0
         }
         
         
 
     }
 
-    function cancelLoan() public {
-        Registry r = Registry(addr);
+    function cancelLoan() internal {
+        Registry r = Registry(Reg);
         r.updatePropertyFromMortgage(creditor,pin);
-
     }
 
 
---
+++ test/repayment.test.js
@@ -0,0 +1,81 @@
+// The public file for automated testing can be found here: https://gist.github.com/ConsenSys-Academy/e9ec0d8d6c53b56ca9673cfa139b5644
+
+var Repayment = artifacts.require('Repayment')
+var Registry = artifacts.require('Registry')
+let catchRevert = require("./utils/exceptionsHelpers.js").catchRevert
+const helper = require('./utils/utils.js');
+const time = require("./utils/timeHelper.js");
+
+const BN = web3.utils.BN
+
+contract('Repayment', function (accounts) {
+
+    const owner = accounts[0]
+    const creditor = accounts[1]
+    const creditee = accounts[2]
+
+
+    beforeEach(async () => {
+        Reg = await Registry.new()
+        contract_addr = Reg.address
+        Repay = await Repayment.new(creditor,creditee,4,2,12,contract_addr)
+        
+    })
+
+    describe("Testing Helper Functions", () => {
+        /*
+        it("should advance the blockchain forward a block", async () =>{
+            const originalBlockHash = await web3.eth.getBlock('latest').hash;
+            console.log(await web3.eth.getBlock('latest'))
+            let newBlockHash = await web3.eth.getBlock('latest');
+            console.log(newBlockHash)
+            console.log(await web3.eth.getBlock('latest'))
+            newBlockHash = await time.advanceBlock();
+            console.log(newBlockHash) 
+            console.log(await web3.eth.getBlock('latest'))
+            assert.notEqual(originalBlockHash, newBlockHash);
+        });
+    */
+        it("should be able to advance time and block together", async () => {
+            const advancement = 600;
+            const originalBlock = await web3.eth.getBlock('latest');
+            const newBlock = await time.advanceTimeAndBlock(advancement);
+            console.log("timestamp originalblock "+originalBlock.timestamp)
+            console.log("timestamp newblock "+newBlock.timestamp)
+            const timeDiff = newBlock.timestamp - originalBlock.timestamp;
+    
+            assert.isTrue(timeDiff >= advancement);
+        });
+    });
+
+    it("Receiving a payment on time", async () =>{
+       // const advancement = 604800; //7days
+        const advancement = 600000;//less than 7days
+        const pay = 0.3*(10**18)
+        //console.log("Due date "+parseInt(await Repay.dueDate()))
+        //console.log("Current time  "+parseInt(await Repay.getNow()))
+        const newBlock = await time.advanceTimeAndBlock(advancement);
+        //console.log("time after going forward "+parseInt(await Repay.getNow()))
+        balancebefore = parseInt(await web3.eth.getBalance(Repay.address))
+        const tId = await Repay.makePayment({from:creditee,value:pay});
+        balanceafter = parseInt(await web3.eth.getBalance(Repay.address))
+        assert.equal(balanceafter,balancebefore+pay,"Checking that the balance of the contract is what the creditee send") 
+    })
+
+    it("Missed Payment so can't accept it", async()=>{
+        const advancement = 604900; //7days+100sec
+        const pay = 0.3*(10**18)
+        const newBlock = await time.advanceTimeAndBlock(advancement);
+        const balanceA = parseInt(await web3.eth.getBalance(creditee))
+        const tId = await Repay.makePayment({from:creditee,value:pay});
+        const tx = await web3.eth.getTransaction(tId.tx);
+        const penalty=parseInt(await Repay.penalty())
+        const balanceB = parseInt(await web3.eth.getBalance(creditee))
+        const diffBalance = balanceA-balanceB
+        const txcost=tId.receipt.cumulativeGasUsed*tx.gasPrice
+        assert.equal(await Repay.missedPayment(),1,"count the missed payment")
+        assert.equal(diffBalance.toString().substring(0,6),(penalty+txcost).toString().substring(0,6)," Balance after is not the right one, should be Balance - fees and transaction cost")  
+    })
+
+
+})
