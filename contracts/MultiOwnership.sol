34c662598066c6d9e22fe0319ba04ec81d5f6efc
Alex-Kevin LOEMBE <alexkevin.loembe@nist.gov>
2020-08-21 13:36:37 -0400

    working on multiownership

--- contracts/MultiOwnership.sol
+++ contracts/MultiOwnership.sol
@@ -5,22 +5,26 @@ contract MultiOwnership{
     address[] public owners;
     bytes32[] public allOperations;
     uint256 public pourcentToPass;
+    uint256 public amountToReach;
+    uint256 public pin;
+    uint256 public pinOwner;
 
     //Reverse lookup to match owners and indices
+    mapping(address => uint) public ownersPct;
     mapping(address => uint) public ownersIndices;
-    mapping(bytes => uint) public allOperationsIndicies
+    mapping(bytes => uint) public allOperationsIndicies;
 
     //Owners voting per operations
     mapping(bytes32 => uint256) public votesMaskByOperation;
     mapping(bytes32 => uint256) public votesCountByOperation;
 
     //Accessors
-    function isOwner() public {
-
+    function isOwner(address _owners) public view returns (bool){
+        return ownersIndices[_owners]>0;
     }
 
-    function ownersCount() public {
-
+    function ownersCount() public view returns(uint) {
+        return owners.length;
     }
 
     function allOperationsCount() public {
@@ -31,28 +35,42 @@ contract MultiOwnership{
 
     }
     
-    function getBalance() public {
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
 
     }
     //Events
 
     //Modifiers
     modifier onlyOwner{
-
+        _;
     }
 
     modifier onlyAllOwner{
+        _;
+    }
 
+    modifier notAchieved{
+        require(address(this).balance<amountToReach);
+        _;
     }
 
     //Constructor
 
-    constructor() public{
-
+    constructor(uint256 _pourcent, uint256 _amount, uint256 _pin, uint256 _pinOwner) public payable{
+        require(msg.value>0);
+        require(msg.value<_amount);
+        amountToReach=_amount;
+        pourcentToPass=_pourcent;
+        pin=_pin;
+        pinOwner=_pinOwner;
+        owners.push(msg.sender);
+        ownersIndices[msg.sender]=owners.length;
+        ownersPct[msg.sender]=(msg.value/amountToReach)*100;
     }
 
     //Internal Methods
-    function checkUpVote(){
+    function checkUpVote() internal {
 
     }
 
@@ -65,21 +83,40 @@ contract MultiOwnership{
     }
     }
 
+    function buyProperty() internal {
+
+    }
+
     
     //Public Methods
-    function buyProperty(){
+    function joinSharedProperty() public payable notAchieved() {
+        require(ownersIndices[msg.sender]==0);
+        require(msg.value>0);
+        require(msg.value<=(amountToReach-address(this).balance));
+        owners.push(msg.sender);
+        ownersIndices[msg.sender]=owners.length;
+        ownersPct[msg.sender]=(msg.value/amountToReach)*100;
+        buyProperty();
+    }
+
+    function addMoney() public payable onlyOwner() notAchieved(){
+        require(msg.value>0);
+        require(msg.value<=(amountToReach-address(this).balance));
+        uint256 tmp=ownersPct[msg.sender];
+        ownersPct[msg.sender]=((msg.value/amountToReach)*100)+tmp;
+        buyProperty();
+    }
+
+
+    function sellShare() public {
 
     }
 
-    function transferOwnership(){
+    function upVote() public {
 
     }
 
-    function upVote(){
-
-    }
-
-    function downVote(){
+    function downVote() public {
 
     }
 
